2d6d46ccb84fea9c2d051951eab473dbb1d440e5
Thomas Verheyden <80472867+v3rtho@users.noreply.github.com>
2026-01-15 20:42:57 +0100

    Add script to update user consent permissions
    This script connects to Microsoft Graph and manages user consent permissions by assigning new permissions and updating existing ones.

---
+++ Update-UserConsentPermissions.ps1
@@ -0,0 +1,57 @@
+#Connect-MgGraph
+
+#Connect-MgGraph -Scopes "DelegatedPermissionGrant.ReadWrite.All", "Directory.Read.All"
+
+<#
+Microsoft Graph 00000003-0000-0000-c000-000000000000
+Office 365 Exchange Online 00000002-0000-0ff1-ce00-000000000000
+SharePoint Online 00000003-0000-0ff1-ce00-000000000000
+#>
+ 
+
+$resourceID = Get-MgServicePrincipal -Filter "displayName eq 'Microsoft Graph'" | Select-Object Id, AppId, DisplayName
+
+$graphSP = Get-MgServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'"
+$graphSP | Select-Object Id, DisplayName, AppId
+
+# Your values
+$clientId = "d9e4f316-fa79-4ae0-ba43-d75119eeed15"  # Object ID from Enterprise apps
+$userId = "c492dfbb-3dcc-4275-902d-ba22d3807067" # Object ID of the user
+$resourceId = $graphSP.Id  # From step 1
+
+# Verify the client (app) exists
+Get-MgServicePrincipal -ServicePrincipalId $clientId | Select-Object Id, DisplayName
+
+# Verify the user exists
+Get-MgUser -UserId $userId | Select-Object Id, DisplayName
+
+# Assign new User consent permission
+
+New-MgOauth2PermissionGrant -ClientId $clientId `
+    -ConsentType "Principal" `
+    -PrincipalId $userId `
+    -ResourceId $resourceId `
+    -Scope "Files.ReadWrite.All offline_access"
+
+
+# Get Service Principal using objectId
+
+$sp = Get-MgServicePrincipal -ServicePrincipalId d9e4f316-fa79-4ae0-ba43-d75119eeed15
+
+# Get all delegated permissions for the service principal
+
+$spOAuth2PermissionsGrants = Get-MgServicePrincipalOauth2PermissionGrant -ServicePrincipalId $sp.Id -All
+ 
+$spOAuth2PermissionsGrants |format-list
+# Remove all delegated permissions
+
+$spOAuth2PermissionsGrants | ForEach-Object {
+
+    #Remove-MgOauth2PermissionGrant -OAuth2PermissionGrantId $_.Id
+    Update-MgOauth2PermissionGrant -OAuth2PermissionGrantId $_.Id -Scope "offline_access"
+
+}
+
+ 
+
+
